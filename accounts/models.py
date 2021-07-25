0aa77ed0ed245046221f42cd4608377145ee055a
Sarthak <sarthak11yadav@gmail.com>
2021-07-25 22:41:01 +0530

    user_customer_relationship

--- accounts/models.py
+++ accounts/models.py
@@ -1,8 +1,11 @@
 from django.db import models
 
+from django.contrib.auth.models import User
+
 # Create your models here.
 
 class Customer(models.Model):
+	user=models.OneToOneField(User,null=True,on_delete=models.CASCADE)
 	name = models.CharField(max_length=200, null=True)
 	phone = models.CharField(max_length=200, null=True)
 	email = models.CharField(max_length=200, null=True)
--- accounts/views.py
+++ accounts/views.py
@@ -28,6 +28,7 @@ def registerPage(request):
 
 			group = Group.objects.get(name='customer')
 			user.groups.add(group)
+			Customer.objects.create(user=user,)
 
 			messages.success(request, 'Account was created for ' + username)
 
@@ -77,8 +78,18 @@ def home(request):
 
 	return render(request, 'accounts/dashboard.html', context)
 
+
+@login_required(login_url='login')
+@allowed_users(allowed_roles=['customer'])
 def userPage(request):
-	context = {}
+	orders=request.user.customer.order_set.all()
+	total_orders = orders.count()
+	delivered = orders.filter(status='Delivered').count()
+	pending = orders.filter(status='Pending').count()
+
+	context = {'orders':orders,
+	'total_orders':total_orders,'delivered':delivered,
+	'pending':pending }
 	return render(request, 'accounts/user.html', context)
 
 
