5cabc9f857399e7bbf220da033233cd608f63ca3
Sarthak <sarthak11yadav@gmail.com>
2021-07-28 21:51:45 +0530

    password_reset

--- crm1/settings.py
+++ crm1/settings.py
@@ -126,3 +126,12 @@ STATICFILES_DIRS = [
     BASE_DIR / "static"
 ]
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+#SMTP Configuration
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'sarthak11yadav@gmail.com'
+EMAIL_HOST_PASSWORD = 'Sar@1234'
